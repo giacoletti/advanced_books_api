630199d7676e940d861146de89eda0cc815b00c6
giacoletti <giacoletti92@gmail.com>
2022-02-27 21:18:50 +0100

    Add request spec for Books delete action

---
+++ specs/requests/books/delete.feature.js
@@ -0,0 +1,26 @@
+const { factory, expect, serverConfig } = require("../../helpers");
+
+let request, response, book;
+
+describe.only("DELETE /api/books/:id", () => {
+  before((done) => {
+    request = serverConfig(done);
+  });
+  
+  afterEach(async () => {
+    await factory.cleanUp();
+  });
+
+  beforeEach(async () => {
+    book = await factory.create("Book", { title: "The Divine Comedy" });
+    response = await request.delete(`/api/books/${book.id}`);
+  });
+
+  it("is expected to respond with status 200", () => {
+    expect(response.status).to.equal(200);
+  });
+
+  it("is expected to include the title", () => {
+    expect(response.body.message).to.equal("Book successfully deleted.");
+  });
+});
